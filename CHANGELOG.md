c47c95e3604f6d24844274b1004143ba8cfdd6a5
cangir <ahmetcangir07@gmail.com>
2020-05-09 18:42:58 +0200

    chore: remove unused files

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+# [0.1.0](https://github.com/cangir/gatsby-starter-tabler/compare/@cangir/gatsby-starter-tabler@0.0.3...@cangir/gatsby-starter-tabler@0.1.0) (2020-05-09)
+
+
+### Features
+
+* gatsby remark images anywhere ([cbc03c4](https://github.com/cangir/gatsby-starter-tabler/commit/cbc03c48191518fe2c31bd33f6d6d732db980f46))
+
+
+
+
+
+## [0.0.3](https://github.com/cangir/gatsby-starter-tabler/compare/@cangir/gatsby-starter-tabler@0.0.2...@cangir/gatsby-starter-tabler@0.0.3) (2020-05-09)
+
+**Note:** Version bump only for package @cangir/gatsby-starter-tabler
+
+
+
+
+
+## 0.0.2 (2020-05-09)
+
+**Note:** Version bump only for package @cangir/gatsby-starter-tabler
---
+++ gatsby-config.js
@@ -0,0 +1,55 @@
+require(`dotenv`).config()
+
+module.exports = {
+  siteMetadata: {
+    title: `Tabler.`,
+    description: `A personal blogging theme for Gatsby with great typography and dark mode.`,
+    siteUrl: process.env.SITE_URL || `http://localhost`,
+    startUrl: `/`,
+    copyright: `© YYYY Tabler. All rights reserved.`,
+    icon: `src/images/icon.png`,
+    color: `#3C64F1`,
+    menuLinks: [
+      {
+        name: `Home`,
+        link: `/`,
+      },
+      {
+        name: `Blog`,
+        link: `/blog`,
+      },
+      {
+        name: `About`,
+        link: `/about`,
+      },
+      {
+        name: `Contact`,
+        link: `/contact`,
+      },
+    ],
+    socialLinks: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com`,
+        icon: `twitter`,
+      },
+      {
+        name: `Github`,
+        url: `https://github.com`,
+        icon: `github`,
+      },
+      {
+        name: `Youtube`,
+        url: `https://youtube.com`,
+        icon: `youtube`,
+      },
+    ],
+  },
+  plugins: [
+    // This is a list of all themes that this starter is using.
+    // To disable a theme, remove it here and run `yarn remove @cangir/gatsby-theme-NAME`.
+    `@cangir/gatsby-theme-blog-core`,
+    `@cangir/gatsby-theme-page-core`,
+    `@cangir/gatsby-theme-tabler`,
+  ],
+}
